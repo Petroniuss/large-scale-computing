ef715bc79ad967d14eb37a3d6bcbfff56f6b00c5
Patryk Wojtyczek <pwojtyczek@qualtrics.com>
2022-10-28 01:00:10 +0200

    move rimrock client to a separate file

--- lab3/src/main.rs
+++ lab3/src/main.rs
@@ -1,144 +1,13 @@
+mod rimrock_client;
+
 extern crate core;
 
-use reqwest::blocking::{Client, Response};
-use reqwest::header::{HeaderMap, HeaderName, HeaderValue, CONTENT_TYPE};
-use reqwest::StatusCode;
-use serde::de::DeserializeOwned;
-use serde::{Deserialize, Serialize};
-use serde_json::Value;
-use std::str::FromStr;
 use std::{env, thread, time};
+use reqwest::blocking::Client;
+use crate::rimrock_client::{ScheduleJobRequestBody, Status};
 
-#[derive(Deserialize, Serialize, Debug)]
-struct ScheduleJobRequestBody<'a> {
-    host: &'a str,
-    script: &'a str,
-}
-
-#[derive(Deserialize, Serialize, Debug)]
-struct SubmitJobResponseBody {
-    job_id: String,
-    stdout_path: String,
-    stderr_path: String,
-    status: Status,
-
-    #[serde(default)]
-    tag: Option<String>,
-}
-
-#[derive(Deserialize, Serialize, Debug)]
-struct GetJobInfoResponseBody {
-    job_id: String,
-    stdout_path: String,
-    stderr_path: String,
-    status: Status,
-
-    #[serde(default)]
-    tag: Option<String>,
-
-    #[serde(default)]
-    nodes: Option<String>,
-
-    #[serde(default)]
-    cores: Option<String>,
-
-    #[serde(default)]
-    start_time: Option<String>,
-
-    #[serde(default)]
-    end_time: Option<String>,
-
-    #[serde(default)]
-    wall_time: Option<String>,
-
-    #[serde(default)]
-    queue_time: Option<String>,
-}
-
-#[derive(Deserialize, Serialize, Debug, Eq, PartialEq)]
-enum Status {
-    QUEUED,
-    FINISHED,
-
-    RUNNING,
-}
-
-fn submit_job(
-    request_body: &ScheduleJobRequestBody,
-    client: &Client,
-    proxy_header: &str,
-) -> SubmitJobResponseBody {
-    let request_body_json = serde_json::to_string_pretty(&request_body).unwrap();
-    println!("Request to /api/jobs: {}", request_body_json);
-
-    let response = client
-        .post("https://submit.plgrid.pl/api/jobs")
-        .headers(default_headers(proxy_header))
-        .body(request_body_json)
-        .send()
-        .unwrap();
-
-    parse_response(response)
-}
-
-fn get_job_info(job_id: &str, client: &Client, proxy_header: &str) -> GetJobInfoResponseBody {
-    let endpoint = format!("https://submit.plgrid.pl/api/jobs/{job_id}");
-    println!("Request to /api/jobs: {}", endpoint);
-
-    let response = client
-        .get(endpoint)
-        .headers(default_headers(proxy_header))
-        .send()
-        .unwrap();
-
-    parse_response(response)
-}
-
-fn parse_response<T>(response: Response) -> T
-where
-    T: DeserializeOwned,
-{
-    let status = response.status();
-    if status.is_success() {
-        success(response)
-    } else {
-        failure(response, status)
-    }
-}
-
-fn success<T>(response: Response) -> T
-where
-    T: DeserializeOwned,
-{
-    let success_body = response.json::<Value>().unwrap();
-    println!("Request success: {:#?}", success_body);
-
-    serde_json::from_value(success_body).unwrap()
-}
-
-fn failure<T>(response: Response, status: StatusCode) -> T
-where
-    T: DeserializeOwned,
-{
-    let failure_body = response.json::<Value>().unwrap();
-    println!(
-        "Request failure - {:#?} : {:#?}",
-        status.to_string(),
-        failure_body
-    );
-
-    panic!();
-}
-
-fn default_headers(proxy_header: &str) -> HeaderMap {
-    return HeaderMap::from_iter([
-        (CONTENT_TYPE, HeaderValue::from_static("application/json")),
-        (
-            HeaderName::from_str(&"PROXY").unwrap(),
-            HeaderValue::from_str(proxy_header).unwrap(),
-        ),
-    ]);
-}
+// todo fix the script, add modules.
+// todo upload the file to ares.
 
 fn main() {
     if let Some(proxy) = env::var_os("PROXY") {
@@ -156,7 +25,7 @@ fn main() {
         ";
 
         let client = Client::new();
-        let submit_job_response = submit_job(
+        let submit_job_response = rimrock_client::submit_job(
             &ScheduleJobRequestBody {
                 host: "ares.cyfronet.pl",
                 script: sample_script,
@@ -166,7 +35,7 @@ fn main() {
         );
 
         loop {
-            let response = get_job_info(&submit_job_response.job_id, &client, &proxy);
+            let response = rimrock_client::get_job_info(&submit_job_response.job_id, &client, &proxy);
 
             if response.status == Status::FINISHED {
                 println!("{:#?}", response);
---
+++ lab3/src/rimrock_client.rs
@@ -0,0 +1,138 @@
+use reqwest::blocking::{Client, Response};
+use reqwest::header::{HeaderMap, HeaderName, HeaderValue, CONTENT_TYPE};
+use reqwest::StatusCode;
+use serde::de::DeserializeOwned;
+use serde::{Deserialize, Serialize};
+use serde_json::Value;
+use std::str::FromStr;
+
+#[derive(Deserialize, Serialize, Debug)]
+pub(crate) struct ScheduleJobRequestBody<'a> {
+    pub(crate) host: &'a str,
+    pub(crate) script: &'a str,
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+pub(crate) struct SubmitJobResponseBody {
+    pub(crate) job_id: String,
+    stdout_path: String,
+    stderr_path: String,
+    status: Status,
+
+    #[serde(default)]
+    tag: Option<String>,
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+pub(crate) struct GetJobInfoResponseBody {
+    job_id: String,
+    stdout_path: String,
+    stderr_path: String,
+    pub(crate) status: Status,
+
+    #[serde(default)]
+    tag: Option<String>,
+
+    #[serde(default)]
+    nodes: Option<String>,
+
+    #[serde(default)]
+    cores: Option<String>,
+
+    #[serde(default)]
+    start_time: Option<String>,
+
+    #[serde(default)]
+    end_time: Option<String>,
+
+    #[serde(default)]
+    wall_time: Option<String>,
+
+    #[serde(default)]
+    queue_time: Option<String>,
+}
+
+#[derive(Deserialize, Serialize, Debug, Eq, PartialEq)]
+pub(crate) enum Status {
+    QUEUED,
+    FINISHED,
+
+    RUNNING,
+}
+
+pub(crate) fn submit_job(
+    request_body: &ScheduleJobRequestBody,
+    client: &Client,
+    proxy_header: &str,
+) -> SubmitJobResponseBody {
+    let request_body_json = serde_json::to_string_pretty(&request_body).unwrap();
+    println!("Request to POST /api/jobs: {}", request_body_json);
+
+    let response = client
+        .post("https://submit.plgrid.pl/api/jobs")
+        .headers(default_headers(proxy_header))
+        .body(request_body_json)
+        .send()
+        .unwrap();
+
+    parse_response(response)
+}
+
+pub(crate) fn get_job_info(job_id: &str, client: &Client, proxy_header: &str) -> GetJobInfoResponseBody {
+    let endpoint = format!("https://submit.plgrid.pl/api/jobs/{job_id}");
+    println!("Request to GET /api/jobs: {}", endpoint);
+
+    let response = client
+        .get(endpoint)
+        .headers(default_headers(proxy_header))
+        .send()
+        .unwrap();
+
+    parse_response(response)
+}
+
+fn parse_response<T>(response: Response) -> T
+    where
+        T: DeserializeOwned,
+{
+    let status = response.status();
+    if status.is_success() {
+        success(response)
+    } else {
+        failure(response, status)
+    }
+}
+
+fn success<T>(response: Response) -> T
+    where
+        T: DeserializeOwned,
+{
+    let success_body = response.json::<Value>().unwrap();
+    println!("Request success: {:#?}", success_body);
+
+    serde_json::from_value(success_body).unwrap()
+}
+
+fn failure<T>(response: Response, status: StatusCode) -> T
+    where
+        T: DeserializeOwned,
+{
+    let failure_body = response.json::<Value>().unwrap();
+    println!(
+        "Request failure - {:#?} : {:#?}",
+        status.to_string(),
+        failure_body
+    );
+
+    panic!();
+}
+
+fn default_headers(proxy_header: &str) -> HeaderMap {
+    return HeaderMap::from_iter([
+        (CONTENT_TYPE, HeaderValue::from_static("application/json")),
+        (
+            HeaderName::from_str(&"PROXY").unwrap(),
+            HeaderValue::from_str(proxy_header).unwrap(),
+        ),
+    ]);
+}
