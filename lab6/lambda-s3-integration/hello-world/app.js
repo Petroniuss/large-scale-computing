dabc2fe3a147ad9732e9ca57950299977c2a0172
Patryk Wojtyczek <pwojtyczek@qualtrics.com>
2022-11-27 15:53:25 +0100

    fix lambda configuration

--- lab6/lambda-s3-integration/hello-world/app.js
+++ lab6/lambda-s3-integration/hello-world/app.js
@@ -4,7 +4,7 @@ const aws = require('aws-sdk');
 
 const s3 = new aws.S3({ apiVersion: '2006-03-01' });
 
-exports.handler = async (event, context) => {
+exports.lambdaHandler = async (event, context) => {
     console.log('Received event:', JSON.stringify(event, null, 4));
     const bucket = event.Records[0].s3.bucket.name;
     const key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '));
