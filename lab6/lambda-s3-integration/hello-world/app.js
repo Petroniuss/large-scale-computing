c14c0459b8e97d6a67e460322db8fd63a86968f9
Patryk Wojtyczek <pwojtyczek@qualtrics.com>
2022-11-27 15:38:49 +0100

    implemented copying an object between buckets

--- lab6/lambda-s3-integration/hello-world/app.js
+++ lab6/lambda-s3-integration/hello-world/app.js
@@ -5,9 +5,7 @@ const aws = require('aws-sdk');
 const s3 = new aws.S3({ apiVersion: '2006-03-01' });
 
 exports.handler = async (event, context) => {
-    console.log('Received event:', JSON.stringify(event, null, 2));
-
-    // Get the object from the event and show its content type
+    console.log('Received event:', JSON.stringify(event, null, 4));
     const bucket = event.Records[0].s3.bucket.name;
     const key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '));
     const params = {
@@ -15,13 +13,25 @@ exports.handler = async (event, context) => {
         Key: key,
     };
     try {
-        const { ContentType } = await s3.getObject(params).promise();
-        console.log('CONTENT TYPE:', ContentType);
-        return ContentType;
+        const { Body } = await s3.getObject(params).promise();
+        let objectData = Body.toString('utf-8');
+
+        const now = new Date().toString();
+        let newObjectData = `${objectData}\n${now}`;
+
+        let destinationBucket = "lsc-test-bucket-1111231231-destination";
+        console.log(`Uploading ${bucket}${key} to ${destinationBucket}${key}.`)
+        await s3.putObject(
+            {
+                Bucket: destinationBucket,
+                Body: newObjectData,
+                Key: key
+            }
+        );
+
+        return 'OK';
     } catch (err) {
         console.log(err);
-        const message = `Error getting object ${key} from bucket ${bucket}. Make sure they exist and your bucket is in the same region as this function.`;
-        console.log(message);
-        throw new Error(message);
+        throw new Error(err);
     }
 };
